547a1623890d255020982f2da18476fba96c5d1b
Rafael-Leitao <rafael.s.leitao@gmail.com>
2024-03-26 12:31:48 -0700

    Initial Commit

---
+++ macroSpendingAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macroSpendingAnalyzer
+//
+//  Created by Marina Marques on 3/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macroSpendingAnalyzer/macroSpendingAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  macroSpendingAnalyzerApp.swift
+//  macroSpendingAnalyzer
+//
+//  Created by Marina Marques on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct macroSpendingAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
