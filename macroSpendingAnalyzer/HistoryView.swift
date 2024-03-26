7819c67934f6559bfe505c4eb4b0887cac4dfec3
Rafael-Leitao <rafael.s.leitao@gmail.com>
2024-03-26 16:06:14 -0700

    Initial page, scan implementation and Scan History started.

---
+++ macroSpendingAnalyzer/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  macroSpendingAnalyzer
+//
+//  Created by Marina Marques on 3/26/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
---
+++ macroSpendingAnalyzer/OCRView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct OCRView: View {
+    var body: some View {
+        // Your OCR functionality goes here
+        Text("OCR View") // Placeholder for now
+    }
+}
+
+struct OCRView_Previews: PreviewProvider {
+    static var previews: some View {
+        OCRView()
+    }
+}
+
