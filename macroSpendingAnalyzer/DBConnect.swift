d1a53b4f3fd50bb2f553ef31ac43b7187e2364ed
Anthony Silva <asilva32@sfsu.edu>
2024-04-08 18:32:33 -0700

    create purchase table

--- macroSpendingAnalyzer/DBConnect.swift
+++ macroSpendingAnalyzer/DBConnect.swift
@@ -26,5 +26,33 @@ class DBConnect {
         }
     }
     
-    
+    func createPurchaseTable() {
+        
+        //optional type 'Connection?' must be unwrapped to refer to member 'run' of wrapped base type 'Connection
+        // TODO check generics
+        guard let db = db else {return}
+        
+        // use optional generics for expressions that can evaluate to null
+        let id  = Expression<Int64>("id")
+        let date = Expression<Date>("date")
+        let business = Expression<String>("business")
+        let catagory =  Expression <String>("category")
+        let product = Expression <String>("product")
+        let total = Expression <Double>("total")
+        
+        let purchases = Table("purchases")
+        
+        do {
+            try db.run(purchases.create {p in
+                p.column(id, primaryKey: .autoincrement)
+                p.column(business)
+                p.column(catagory)
+                p.column(product)
+                p.column(total)
+                p.column(date)
+            })
+        }catch {
+            print(error)
+        }
+    }
 }
