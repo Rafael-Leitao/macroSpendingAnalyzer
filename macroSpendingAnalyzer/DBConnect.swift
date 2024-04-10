bade99ad707c7398c95736e95ef2635b8918480d
Anthony Silva <asilva32@sfsu.edu>
2024-04-09 20:21:30 -0700

    fixed project set up

--- macroSpendingAnalyzer/DBConnect.swift
+++ macroSpendingAnalyzer/DBConnect.swift
@@ -11,8 +11,16 @@ import SQLite
 
 class DBConnect {
     private var db: Connection?
+    let id  = Expression<Int64>("id")
+    let date = Expression<Date>("date")
+    let business = Expression<String>("business")
+    let category =  Expression <String>("category")
+    let product = Expression <String>("product")
+    let total = Expression <Double>("total")
     
-    // Create new database or establish connection
+    let purchases = Table("purchases")
+    // Create new database or establish connection using app documents directory
+    
     init() {
         do {
             let path = NSSearchPathForDirectoriesInDomains(
@@ -20,6 +28,7 @@ class DBConnect {
             ).first!
             
             self.db = try Connection("\(path)/db.sqlite3")
+            
             print("Connection established")
         } catch {
             print(error)
@@ -33,20 +42,14 @@ class DBConnect {
         guard let db = db else {return}
         
         // use optional generics for expressions that can evaluate to null
-        let id  = Expression<Int64>("id")
-        let date = Expression<Date>("date")
-        let business = Expression<String>("business")
-        let catagory =  Expression <String>("category")
-        let product = Expression <String>("product")
-        let total = Expression <Double>("total")
+
         
-        let purchases = Table("purchases")
-        
+        // TODO check that the table hasn't already been created
         do {
             try db.run(purchases.create {p in
                 p.column(id, primaryKey: .autoincrement)
                 p.column(business)
-                p.column(catagory)
+                p.column(category)
                 p.column(product)
                 p.column(total)
                 p.column(date)
@@ -55,4 +58,38 @@ class DBConnect {
             print(error)
         }
     }
+    
+    // Function to insert a purchase record
+    func insertPurchase(_ business: String, category: String, product: String, total: Double, date: Date) {
+        guard let db = db else {return}
+        let insert = purchases.insert(
+            self.business <- business,
+            self.category <- category,
+            self.product <- product,
+            self.total <- total,
+            self.date <- date
+                    )
+        
+        do {
+            let insertRow = try db.run(insert)
+            print("Purchase inserted successfully to row \(insertRow).")
+        } catch {
+            print("Error inserting purchase: \(error)")
+        }
+    }
+
+    func removePurchase(purchaseID: Int64) {
+        guard let db = db else {return}
+        do {
+            let purchaseToDelete = purchases.filter(id == purchaseID)
+            if try db.run(purchaseToDelete.delete()) > 0 {
+                print("Purchase with ID \(purchaseID) removed successfully.")
+            } else {
+                print("Purchase with ID \(purchaseID) not found.")
+            }
+        } catch {
+            print("Error removing purchase: \(error)")
+        }
+    }
+    
 }
--- macroSpendingAnalyzer/receipt.swift
+++ macroSpendingAnalyzer/receipt.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+import Foundation
+
+struct Receipt {
+  //  var id: Int64
+    var date: Date
+    var business: String
+    var catagory: String
+    var product: String
+    var total: Double
+}
